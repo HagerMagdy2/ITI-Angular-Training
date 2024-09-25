66dc5531cbce4caa60eeef39515ee18722c77c0e
HagerMagdy2 <hager9671@gmail.com>
2024-09-25 18:10:21 +0300

    commit

---
+++ Day09/Task01/script.js
@@ -0,0 +1,10 @@
+let card = document.getElementsByClassName('card');
+let left =document.querySelector('.left')
+let right =document.getElementsByClassName('right')
+let play =document.getElementsByClassName('play')
+let stop =document.getElementsByClassName('stop')
+
+left.onclick=function (){
+console.log("left")
+}
+console.log(left);
